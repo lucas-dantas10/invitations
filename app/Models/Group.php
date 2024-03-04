f0583ee7f22881bc84b805204d9b72b5dddbdc88
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-03-04 00:07:55 -0300

    feat: add migration and relations

---
+++ app/Models/Group.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Group extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'user_id', 'created_at', 'updated_at'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+}
---
+++ database/migrations/2024_03_04_025834_create_group_invitations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group_invitations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index()->constrained();
+            $table->foreignId('group_id')->index()->constrained();
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group_invitations');
+    }
+};
